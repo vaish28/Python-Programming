cb8e00c957ca26ba38927ed3a200fdd7e2f789ec
Vaishnavi Madhekar <43900410+vaish28@users.noreply.github.com>
2020-12-05 00:50:08 +0530

    Create Button_demp.py

---
+++ Tkinter/Button_demp.py
@@ -0,0 +1,10 @@
+
+
+from tkinter import *
+root= Tk()
+
+root.title("My First Window")
+root.geometry("100x100")
+btn = Button(root,text = "Hi I am a button",fg="purple",command=root.destroy) #kills the button after it is pressed
+btn.pack()
+root.mainloop()
