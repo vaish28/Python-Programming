4ef7e75effcbcc529fa55e411508ef54d50162e5
Vaishnavi Madhekar <43900410+vaish28@users.noreply.github.com>
2021-05-29 17:47:36 +0530

    Update ex_for_tokenizer.py

--- JOC-Python/NLP_Stylometry/ex_for_tokenizer.py
+++ JOC-Python/NLP_Stylometry/ex_for_tokenizer.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr 12 16:00:50 2020
 
-@author: Supriya
 """
 #import nltk     # use any one of import statements if this statement is used then, tokens=nltk.word_tokenize(text)   to be used
 from nltk import word_tokenize
