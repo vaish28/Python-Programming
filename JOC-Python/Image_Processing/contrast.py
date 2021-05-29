898c2222bf9f0f0191b878af4662f0bc2077238a
Vaishnavi Madhekar <43900410+vaish28@users.noreply.github.com>
2021-05-29 17:12:05 +0530

    Add files via upload

---
+++ JOC-Python/Image_Processing/contrast.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  3 07:14:37 2021
+
+@author: User
+"""
+'''
+from PIL import Image,ImageEnhance
+
+img = Image.open('C:/Users/User/Desktop/OpenCV/resources/photos/blur1.png')
+
+img_contrast = ImageEnhance.Contrast(img)
+
+img_contrast.enhance(2.0).save("contast_blur.png")
+'''
+
+
+from PIL import Image
+from PIL import ImageEnhance
+
+image = Image.open("C:/Users/User/Desktop/OpenCV/resources/photos/blur1.png")
+image.show()
+enhancer = ImageEnhance.Contrast(image)
+new_image = enhancer.enhance(1.5)
+
+new_image.save("newImage.png")
+new_image.show()
